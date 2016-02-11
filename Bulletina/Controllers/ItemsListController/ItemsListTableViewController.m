5da5aec6496aa932778086975b663c90a9ebc594
Stas Volskyi <stas.volskiy@thinkmobiles.com>
2016-02-11 15:52:16 +0200

    conflict fix

--- Bulletina/Controllers/ItemsListController/ItemsListTableViewController.m
+++ Bulletina/Controllers/ItemsListController/ItemsListTableViewController.m
@@ -35,11 +35,7 @@ static CGFloat const priceContainerHeigth = 43.0f;
 		[self.loader show];
 	}
 	__weak typeof(self) weakSelf = self;
-<<<<<<< HEAD
 	[[APIClient sharedInstance] fetchItemsWithOffset:@50 limit:@85 withCompletion:^(id response, NSError *error, NSInteger statusCode) {
-=======
-	[[APIClient sharedInstance] fetchItemsWithOffset:@0 limit:@80 withCompletion:^(id response, NSError *error, NSInteger statusCode) {
->>>>>>> origin/API
 		if (error) {
 			if (response[@"error_message"]) {
 				[Utils showErrorWithMessage:response[@"error_message"]];
